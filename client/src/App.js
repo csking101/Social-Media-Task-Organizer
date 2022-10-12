22173cbcdf56fdbd27f14e552ba5c499a2c6892c
csking101 <42250012+csking101@users.noreply.github.com>
2022-10-12 13:56:55 +0530

    Setup the basic structure of the react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,20 @@
-import './App.css';
+import "./App.css";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+            <Route path="/login" element={<Login />}>
+            <Route path="/" element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/pages/Login.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+import Loginbox from "../components/Loginbox";
+
+const Login = () => {
+  return (
+    <div>
+      <Loginbox />
+    </div>
+  );
+};
+
+export default Login;
