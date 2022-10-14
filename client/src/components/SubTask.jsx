4b571ced956d1391b41064aff787c0653220e16a
csking101 <42250012+csking101@users.noreply.github.com>
2022-10-14 11:17:06 +0530

    Created the SubTask card component

---
+++ client/src/components/SubTask.jsx
@@ -0,0 +1,71 @@
+import { styled, Card, Typography } from "@mui/material";
+import React from "react";
+
+const CardContainer = styled(Card)`
+  background-color: #04151f;
+  padding: 15px;
+  margin-left: 15px;
+  margin-right: 15px;
+  margin-bottom: 15px;
+`;
+
+const Heading = styled(Typography)`
+  font-size: 30px;
+  color: #39a2ae;
+`;
+
+const Deadline = styled(Typography)`
+  font-size: 20px;
+  color: #ea3546;
+`;
+
+const Platform = styled(Typography)`
+  font-size: 20px;
+  color: #ff9000;
+`;
+
+const CreatedBy = styled(Typography)`
+  font-size: 14px;
+  color: #39a2ae;
+`;
+
+const Description = styled(Typography)`
+  font-size: 14px;
+  color: #39a2ae;
+`;
+
+const FileAttached = styled(Typography)`
+  font-size: 14px;
+  color: #FF9000;
+`;
+
+const Status = styled(Typography)`
+  font-size: 14px;
+  color: #ea3546;
+`;
+
+const SubTask = (props) => {
+  let file;
+  if (!props.file) file = "None";
+  else file = props.file;
+
+  console.log(props);
+
+  return (
+    <CardContainer>
+      <Heading>{props.title.toUpperCase()}</Heading>
+      <Status>
+        <b>STATUS:</b> {props.status}
+      </Status>
+      <Deadline>{props.deadline.toString().slice(0, 10)}</Deadline>
+      <Platform>{props.platform}</Platform>
+      <CreatedBy>{props.createdby}</CreatedBy>
+      <Description>{props.desc}</Description>
+      <FileAttached>
+        <b>ATTACHMENTS:</b> {file}
+      </FileAttached>
+    </CardContainer>
+  );
+};
+
+export default SubTask;
