8e36a13fddae66b5013fcc539d3696b623c016e9
csking101 <42250012+csking101@users.noreply.github.com>
2022-10-15 12:21:39 +0530

    Home page redirects to Login page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,8 @@
 import React from "react";
+import { Navigate } from "react-router-dom";
 
-function Home() {
-  return <div>This is home</div>;
-}
+const Home = () => {
+  return <Navigate to="/login" />;
+};
 
 export default Home;
